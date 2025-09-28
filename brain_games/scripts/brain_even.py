4d028bc81baf80c6f7f5f316ec36e10697e86fed
Джамал Абсаидов <dzamalabsaidov@MacBook-Pro-Dzamal.local>
2025-09-28 20:27:48 +0300

    доделал шаг 5

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -3,19 +3,18 @@ import prompt
 
 
 def main():
-    random_number: int = random.randint(0, 100)
-    player_name: str = prompt.string('May I have your name? ')
-    description: str = f'Answer "yes" if the number is even, otherwise answer "no".'
-
     print(f'Welcome to the Brain Games!')
+    player_name: str = prompt.string('May I have your name? ')
     print(f'Hello, {player_name}!')
+    print(f'Answer "yes" if the number is even, otherwise answer "no".')
+
     for i in range(0, 3):
-        print(description)
+        random_number: int = random.randint(0, 99)
         print(f'Question {random_number}')
         answer_from_console = prompt.string('Your answer: ')
         print(answer_from_console)
         is_even_or_not: str = 'yes' if is_even(random_number) else 'no'
-        if answer_from_console == is_even_or_not:
+        if answer_from_console.lower() == is_even_or_not:
             print(f'Correct!')
         else:
             print(f"""{answer_from_console} is wrong answer ;(. Correct answer was {is_even_or_not}.
